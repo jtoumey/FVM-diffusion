a0fb68a6347959f749419e8bcd650f27b8c1823c
jtoumey <jtoumey@gmail.com>
2015-01-26 20:19:37 -0600

    tracking all f90 files, added variables

---
+++ diffusion1d.f90
@@ -0,0 +1,31 @@
+!*************************************************************************!
+!                                                                         !
+!  Module:       DIFFUSION1D.F90                                          !
+!                                                                         !
+!  Programmer:   Julian M. Toumey                                         !
+!                Madison, WI                                              !
+!                                                                         !
+!  Date:         26 January 2015                                          !
+!                                                                         !
+!  Language:     FORTRAN90                                                !
+!                                                                         !
+!  Description:  This code solves source-free heat conduction in a 1-D    !
+!                bar. The method follows example 4.3 in Versteeg and      !
+!                Malalasekera, 2nd ed.                                    !
+!                                                                         !
+!*************************************************************************!
+
+PROGRAM DIFFUSION1D
+
+parameter (n = 5)
+double precision k,A
+
+
+k = 1000. 
+A 
+ 
+
+
+
+END
+
