01143b479fd2d1adc98d2b9770d272c00cae6ddc
jtoumey <jtoumey@gmail.com>
2015-01-27 17:52:19 -0600

    add files for example 4.2 in V&M

---
+++ 4_2/difsrc1d.f90
@@ -0,0 +1,23 @@
+!*************************************************************************!
+!                                                                         !
+!  Module:       DIFSRC1D.F90                                             !
+!                                                                         !
+!  Programmer:   Julian M. Toumey                                         !
+!                Madison, WI                                              !
+!                                                                         !
+!  Date:         27 January 2015                                          !
+!                                                                         !
+!  Language:     FORTRAN90                                                !
+!                                                                         !
+!  Description:  This code solves heat conduction in a 1-D plate with a   !
+!                constant source. The method follows example 4.3 in       !
+!                Versteeg and Malalasekera, 2nd ed.                       !
+!                                                                         !
+!*************************************************************************!
+PROGRAM DIFSRC1D
+IMPLICIT NONE
+
+
+
+
+END
