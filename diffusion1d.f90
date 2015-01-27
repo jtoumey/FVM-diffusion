de29e1bb0faf68874cfe8966fd8c5757cc159e70
jtoumey <jtoumey@gmail.com>
2015-01-27 00:04:13 -0600

    added write to file and screen capability, compiles but bad results

--- diffusion1d.f90
+++ diffusion1d.f90
@@ -87,7 +87,14 @@ call thomas(n,a,b,c,d,T)
 !
 !...Write results
 !
-
+      write(6,101)
+101   format(5x,'____x(j)___',3x,'___phi(j)___')
+      open(unit=7,file='ps3_1.dat')
+      do jj = 1,n
+         write(6,201)x(jj),T(jj)
+         write(7,201)x(jj),T(jj)
+201      format(3x,f12.5,3x,f12.5)
+      end do
 
 
 
