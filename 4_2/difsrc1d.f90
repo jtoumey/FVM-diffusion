217eb4780f26b6ec77705194b9255b9a1f2cc81f
jtoumey <jtoumey@gmail.com>
2015-01-27 19:34:46 -0600

    create grid

--- 4_2/difsrc1d.f90
+++ 4_2/difsrc1d.f90
@@ -17,7 +17,36 @@
 PROGRAM DIFSRC1D
 IMPLICIT NONE
 
+integer n,ii,jj
+parameter (n = 5)
+real k,q,area,Ta,Tb,T(n)
+real xmax,x(n),dx
+real aw,ap,ae,Su,Sp
+real a(n),b(n),c(n),d(n)
+!
+!...thermal conductivity [W/m.K] and uniform heat generation [kW/m^3]
+!   area [m^2]
+!
+k    = 0.05
+q    = 1000000.
+area = 1.
+!
+!...boundary temperatures: fixed [*C]
+!
+Ta = 100.
+Tb = 200.
+!
+!...set up the grid
+!   xmax is the domain length [m]
+!
+xmax = 0.02
+dx = xmax/float(n)
+do jj = 1,n
+  x(jj) = (jj-0.5)*dx
+end do
+write(6,201)
 
 
+201      format(3x,f12.5)!,3x,f12.5)
 
 END
