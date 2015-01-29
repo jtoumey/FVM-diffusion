43bb8cab6330cab836a833356fd3fb9d83351e2d
jtoumey <jtoumey@gmail.com>
2015-01-28 18:05:00 -0600

    initial commit on diffusion cooling file from V&M ex.4.3

--- 4_2/difsrc1d.f90
+++ 4_2/difsrc1d.f90
@@ -10,7 +10,7 @@
 !  Language:     FORTRAN90                                                !
 !                                                                         !
 !  Description:  This code solves heat conduction in a 1-D plate with a   !
-!                constant source. The method follows example 4.3 in       !
+!                constant source. The method follows example 4.2 in       !
 !                Versteeg and Malalasekera, 2nd ed.                       !
 !                                                                         !
 !*************************************************************************!
---
+++ 4_3/difcool1d.f90
@@ -0,0 +1,22 @@
+!*************************************************************************!
+!                                                                         !
+!  Module:       DIFCOOL1D.F90                                            !
+!                                                                         !
+!  Programmer:   Julian M. Toumey                                         !
+!                Madison, WI                                              !
+!                                                                         !
+!  Date:         28 January 2015                                          !
+!                                                                         !
+!  Language:     FORTRAN90                                                !
+!                                                                         !
+!  Description:  This code solves cooling of a circular fin by convective !
+!                heat transfer along its length. Base (left boundary) has !
+!                a fixed temperature. Right boundary is insulated (zero   !
+!                normal gradient). The method follows example 4.3 in      !
+!                Versteeg and Malalasekera, 2nd ed.                       !
+!                                                                         !
+!*************************************************************************!
+PROGRAM DIFCOOL1D
+
+
+END
