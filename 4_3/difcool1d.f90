c5a4ca3bf4bbbec7d364c45a514fe484cc655429
jtoumey <jtoumey@gmail.com>
2015-01-28 18:10:47 -0600

    set up the system variables and the grid

--- 4_3/difcool1d.f90
+++ 4_3/difcool1d.f90
@@ -17,6 +17,36 @@
 !                                                                         !
 !*************************************************************************!
 PROGRAM DIFCOOL1D
+IMPLICIT NONE 
 
+integer n,ii,jj
+parameter (n = 5)
+real n2,Ta,Tinf,T(n)
+real xmax,x(n),dx
+
+
+real aw,ap,ae,Su,Sp
+real a(n),b(n),c(n),d(n)
+!
+!...n^2 = hP/kA [1/m^2]
+!
+n2 = 1./25.
+!
+!...boundary temperature: fixed at left [*C]
+!
+Ta = 100.
+!
+!...set up the grid
+!   xmax is the domain length [m]
+!
+xmax = 1.
+dx = xmax/float(n)
+do jj = 1,n
+  x(jj) = (jj-0.5)*dx
+end do
+!
+!...set up system of equations
+!   Left Boundary:
+!
 
 END
