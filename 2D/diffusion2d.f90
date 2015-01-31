61455336556b4cd1da045b3176ae844a698fb18c
jtoumey <jtoumey@gmail.com>
2015-01-30 19:22:51 -0600

    added .gitignore for builds

---
+++ 2D/diffusion2d.f90
@@ -0,0 +1,23 @@
+!*************************************************************************!
+!                                                                         !
+!  Module:       DIFFUSION2D.F90                                          !
+!                                                                         !
+!  Programmer:   Julian M. Toumey                                         !
+!                Madison, WI                                              !
+!                                                                         !
+!  Date:         January 2015                                             !
+!                                                                         !
+!  Language:     FORTRAN90                                                !
+!                                                                         !
+!  Description:  This code solves heat conduction in a 1-D plate with a   !
+!                constant source. The method follows example 4.2 in       !
+!                Versteeg and Malalasekera, 2nd ed.                       !
+!                                                                         !
+!*************************************************************************!
+PROGRAM DIFFUSION2D
+IMPLICIT NONE
+
+
+
+
+END
