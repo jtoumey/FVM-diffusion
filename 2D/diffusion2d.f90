91e3d92faf9bbb015c9d082424ab0149cbe29765
jtoumey <jtoumey@gmail.com>
2015-01-31 17:29:52 -0600

    set up the left-most line to solve with TDMA

--- 2D/diffusion2d.f90
+++ 2D/diffusion2d.f90
@@ -22,43 +22,91 @@ IMPLICIT NONE
 ! implementation)
 
 integer IL,JL,ii,jj,kk
-parameter (IL=6,JL=6)
+parameter (IL=3,JL=4)
 real dx,dy,xmax,ymax,x(IL),y(JL)
+real aw,ae,as,an,Su,Sp,ap
+real qw,k,area,Tn
+real a(JL
+real, dimension(JL,1) :: T
 
 
-real, dimension(IL,JL) :: T
+T = 0.
 !
 !...set up the grid
 !
 xmax = 0.3
-dx = xmax/float(IL-2)
-x(1) = 0.
-do ii = 2,IL-1
-   x(ii) = (ii-1-0.5)*dx
+dx = xmax/float(IL)
+!x(1) = 0.
+do ii = 1,IL
+   x(ii) = (ii-0.5)*dx
 end do
-x(IL) = xmax
+!x(IL) = xmax
 !
 ymax = 0.4
-dy = ymax/float(JL-2)
-y(1) = 0
-do jj = 2,JL-1
-   y(jj) = (jj-1-0.5)*dy
+dy = ymax/float(JL)
+!y(1) = 0
+do jj = 2,JL
+   y(jj) = (jj-0.5)*dy
 end do
-y(JL) = ymax
+!y(JL) = ymax
+!
+!...physical properties
+!
+qw = 500000.
+k = 1000.
+Tn = 100.
 !
 !...Set up system 
+!   SW Corner
+aw = 0.
+ae = k*area/dx
+as = 0.
+an = k*area/dx
+Sp = 0
+Su = area*qw
+ap = aw + ae + as + an + Sp
 !
-
-
-
+a(1) =  0.
+b(1) =  ap
+c(1) = -an
+d(1) =  Su + ae*T(2,1)
+!
+do jj = 2,JL-1
+   aw = 0
+   ae = k*area/dx
+   as = k*area/dx
+   an = k*area/dx
+   Sp = 0
+   Su = area*qw
+   ap = aw + ae + as + an + Sp
+   !
+   a(1) = -as
+   b(1) =  ap
+   c(1) = -an
+   d(1) =  Su + ae*T(2,JL)
+end do
+!...NW corner
+aw = 0.
+ae = k*area/dx
+as = 0.
+an = k*area/dx
+Sp = 0
+Su = area*qw
+ap = aw + ae + as + an + Sp
+!
+a(1) =  0.
+b(1) =  ap
+c(1) = -an
+d(1) =  Su + ae*T(2,1)
+!
 
 
 
 101 format(3x,f12.5,3x,f12.5)
 
-do kk = 1,IL 
-   write(6,101)x(kk),y(kk)
-end do
+!do kk = 1,IL 
+!   write(6,101)x(kk),y(kk)
+!end do
 
 
 END
