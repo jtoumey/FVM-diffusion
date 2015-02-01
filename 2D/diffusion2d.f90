85526f0fd626fab5ab57cb43ab2f1feb02b44a00
jtoumey <jtoumey@gmail.com>
2015-01-31 23:42:45 -0600

    cleaned up the code and improved commenting

--- 2D/diffusion2d.f90
+++ 2D/diffusion2d.f90
@@ -20,7 +20,7 @@ PROGRAM DIFFUSION2D
 IMPLICIT NONE
 !
 integer IL,JL,ii,jj,kk,iter
-parameter (IL=3,JL=4)
+parameter (IL=3000,JL=4000)
 real dx,dy,xmax,ymax,x(IL),y(JL)
 real aw,ae,as,an,Su,Sp,ap
 real qw,k,area,Tn
@@ -28,39 +28,48 @@ real a(JL),b(JL),c(JL),d(JL)
 real Tsol(JL),resid
 real, dimension(JL,IL) :: T,Tprev
 !
-!...Initial temperature distribution: 0 [*C] everywhere
+!...Parameters for iteration
 !
-T = 0.
 resid = 1000.
-iter = 1
+iter  = 1
 !
 !...set up the grid
 !
 xmax = 0.3
-dx = xmax/float(IL)
+dx   = xmax/float(IL)
 do ii = 1,IL
    x(ii) = (ii-0.5)*dx
 end do
 !
 ymax = 0.4
-dy = ymax/float(JL)
+dy   = ymax/float(JL)
 do jj = 2,JL
    y(jj) = (jj-0.5)*dy
 end do
 !
 !...physical properties
+!   West heat flux [W/m^2], therm. conduct. [W/m.K], North fixed temp [*C]
+!   plate thickness 1 cm, area [m^2], initial temperature 0 [*C] everywhere
 !
-qw = 500000.
-k  = 1000.
-Tn = 100.
+qw   = 500000.
+k    = 1000.
+Tn   = 100.
 area = 0.001
-
+T    = 0.
+!--------------------------------------------------------------------------!
+!
+!...Begin outer WHILE iterative loop 
+!
+!--------------------------------------------------------------------------!
 do while (resid >= .01)
+   !   save previous temperature distribution to compare errors
    Tprev = T
+   !-----------------------------------------------------------------------!
    !
    !...West Cells
    !
-   !...Set up system 
+   !-----------------------------------------------------------------------!
+   !
    !   SW Corner
    aw = 0.
    ae = k*area/dx
@@ -74,7 +83,7 @@ do while (resid >= .01)
    b(1) =  ap
    c(1) = -an
    d(1) =  Su + ae*T(1,2)
-   !
+   !   West Interior cells
    do jj = 2,JL-1
       aw = 0.
       ae = k*area/dx
@@ -89,88 +98,85 @@ do while (resid >= .01)
       c(jj) = -an
       d(jj) =  Su + ae*T(jj,2)
    end do
-   !...NW corner
-   aw = 0.
-   ae = k*area/dx
-   as = k*area/dx
-   an = 0.
+   !   NW corner
+   aw =  0.
+   ae =  k*area/dx
+   as =  k*area/dx
+   an =  0.
    Sp = -2.*k*area/dx
-   Su = area*qw + 2.*k*area*Tn/dx
-   ap = aw + ae + as + an - Sp
-   !
-   a(JL) = -as
-   b(JL) =  ap
-   c(JL) = an
-   d(JL) =  Su + ae*T(JL,2)
-
-   !
-   !...solve system with the TDMA
-   !
-   call thomas(JL,a,b,c,d,Tsol)
-   !...Store temperature solution
-T(:,1) = Tsol
-
-
-do ii = 2,IL-1
-   !-----------------------------------------------------------------------!
-   !
-   !...Interior cells
-   !
-   !...Set up system 
-   !   South boundary
-   aw = k*area/dx
-   ae = k*area/dx
-   as = 0.
-   an = k*area/dx
-   Sp = 0
-   Su = 0
-   ap = aw + ae + as + an - Sp
-   !
-   a(1) = -as
-   b(1) =  ap
-   c(1) = -an
-   d(1) =  Su + ae*T(1,ii+1) + aw*T(1,ii-1)
-   !
-   do jj = 2,JL-1
-      aw = k*area/dx
-      ae = k*area/dx
-      as = k*area/dx
-      an = k*area/dx
-      Sp = 0.
-      Su = 0.
-      ap = aw + ae + as + an - Sp
-      !
-      a(jj) = -as
-      b(jj) =  ap
-      c(jj) = -an
-      d(jj) =  Su + ae*T(jj,ii+1) + aw*T(jj,ii-1)
-   end do
-   !...North boundary
-   aw = k*area/dx
-   ae = k*area/dx
-   as = k*area/dx
-   an = 0.
-   Sp = -2.*k*area/dx
-   Su =  2.*k*area*Tn/dx
-   ap = aw + ae + as + an - Sp
+   Su =  area*qw + 2.*k*area*Tn/dx
+   ap =  aw + ae + as + an - Sp
    !
    a(JL) = -as
    b(JL) =  ap
    c(JL) = -an
-   d(JL) =  Su + ae*T(jj,ii+1) + aw*T(jj,ii-1)
+   d(JL) =  Su + ae*T(JL,2)
    !
-   !...solve system with the TDMA
+   !...solve N-S system with the TDMA
    !
    call thomas(JL,a,b,c,d,Tsol)
    !...Store temperature solution
-   T(:,ii) = Tsol
-
-end do
-
+   T(:,1) = Tsol
+   !-----------------------------------------------------------------------!
+   !
+   !...Interior Cells, marching W to E
+   !
+   !-----------------------------------------------------------------------!
+   do ii = 2,IL-1
+      !   South boundary
+      aw = k*area/dx
+      ae = k*area/dx
+      as = 0.
+      an = k*area/dx
+      Sp = 0
+      Su = 0
+      ap = aw + ae + as + an - Sp
+      !
+      a(1) = -as
+      b(1) =  ap
+      c(1) = -an
+      d(1) =  Su + ae*T(1,ii+1) + aw*T(1,ii-1)
+      !
+      do jj = 2,JL-1
+         aw = k*area/dx
+         ae = k*area/dx
+         as = k*area/dx
+         an = k*area/dx
+         Sp = 0.
+         Su = 0.
+         ap = aw + ae + as + an - Sp
+         !
+         a(jj) = -as
+         b(jj) =  ap
+         c(jj) = -an
+         d(jj) =  Su + ae*T(jj,ii+1) + aw*T(jj,ii-1)
+      end do
+      !...North boundary
+      aw =  k*area/dx
+      ae =  k*area/dx
+      as =  k*area/dx
+      an =  0.
+      Sp = -2.*k*area/dx
+      Su =  2.*k*area*Tn/dx
+      ap = aw + ae + as + an - Sp
+      !
+      a(JL) = -as
+      b(JL) =  ap
+      c(JL) = -an
+      d(JL) =  Su + ae*T(jj,ii+1) + aw*T(jj,ii-1)
+      !
+      !...solve N-S system with the TDMA
+      !
+      call thomas(JL,a,b,c,d,Tsol)
+      !...Store temperature solution
+      T(:,ii) = Tsol
+   end do
+   !-----------------------------------------------------------------------!
    !
    !...East Cells
    !
-   !...Set up system 
+   !-----------------------------------------------------------------------!
+   !
    !   SE Corner
    aw = k*area/dx
    ae = 0.
@@ -200,13 +206,13 @@ end do
       d(jj) =  Su + aw*T(jj,IL-1)
    end do
    !...NE corner
-   aw = k*area/dx
-   ae = 0.
-   as = k*area/dx
-   an = 0.
+   aw =  k*area/dx
+   ae =  0.
+   as =  k*area/dx
+   an =  0.
    Sp = -2.*k*area/dx
-   Su = 2.*k*area*Tn/dx
-   ap = aw + ae + as + an - Sp
+   Su =  2.*k*area*Tn/dx
+   ap =  aw + ae + as + an - Sp
    !
    a(JL) = -as
    b(JL) =  ap
@@ -216,23 +222,24 @@ end do
    !...solve system with the TDMA
    !
    call thomas(JL,a,b,c,d,Tsol)
-   !...Store temperature solution
+   !...Store N-S temperature solution
    T(:,IL) = Tsol
-
-
+   !
+   !...Recompute the error, increment the iteration
+   !
    resid = maxval(abs(T - Tprev))
-   write(6,*)
-   write(6,401)iter
    iter = iter + 1
-   
 end do
-
-write(6,201)T
+!--------------------------------------------------------------------------!
+!
+!...Write the results to a file
+!
+!--------------------------------------------------------------------------!
 open(unit=7,file='plate_temp.dat',ACTION="write", STATUS="replace")
 do jj = JL,1,-1
    write(7,'(1000f12.5)') (T(jj,ii),ii=1,IL)
 end do
 
 201 format(3x,f12.5)
-401 format(3x,'*** Iteration :',i4,'  ***')
+401 format(3x,'*** Iteration : ',i4,3x,'Residual :',f12.5,'  ***')
 END
